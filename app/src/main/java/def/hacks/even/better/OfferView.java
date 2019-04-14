41e6f8f2f00aa7928204c04d6a96becd13581d91
William Zulueta <williamzulueta@rocketmail.com>
2019-04-14 03:53:32 -0700

    EvenBetter - style fixes

--- app/src/main/java/def/hacks/even/better/OfferView.java
+++ app/src/main/java/def/hacks/even/better/OfferView.java
@@ -16,17 +16,27 @@ public class OfferView extends LinearLayout {
     private View mainView;
     private TextView offerAmount;
     private TextView offerOrigin;
+    private TextView offerApr;
+    private TextView offerDuration;
 
     public OfferView(Context context) {
         super(context);
         mainView = View.inflate(context, R.layout.view_offer, null);
-        offerAmount = mainView.findViewById(R.id.offerAmount);
-        offerOrigin = mainView.findViewById(R.id.offerOrigin);
+        offerAmount = mainView.findViewById(R.id.offerLoanAmount);
+        offerOrigin = mainView.findViewById(R.id.offerLoanerName);
+        offerDuration = mainView.findViewById(R.id.offerLoanDuration);
+        offerApr = mainView.findViewById(R.id.offerLoanApproved);
         addView(mainView, new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
     }
 
     public void bind(LoanOffer loanOffer) {
         offerAmount.setText(String.valueOf(loanOffer.maxAmount));
         offerOrigin.setText(loanOffer.originator.name);
+        offerDuration.setText(String.valueOf(loanOffer.termLength));
+        offerApr.setText(toYesNo(loanOffer.preApproved));
+    }
+
+    private static final String toYesNo(boolean val) {
+        return (val) ? "yes" : "no";
     }
 }
