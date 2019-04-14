6d25545d0bf690a641546927c57e6ef1e5fb37aa
Darren <37914187+darren-w-huang@users.noreply.github.com>
2019-04-13 21:04:14 -0700

    Update #1

--- backend/site.js
+++ backend/site.js
@@ -28,35 +28,52 @@ app.get('/', (req, res) => {
 }
 });
 */
-const uuid;
+let uuid;
+let getUrl;
 app.use('/', router);
 app.listen(process.env.port || 3000);
-request.post(
-    { 
-    	url: url,
-    	headers: {
-    		'Authorization': 'Bearer e7675dd3-ff3b-434b-95aa-70251cc3784b_88140dd4-f13e-4ce3-8322-6eaf2ee9a2d2',
-        'Accept': 'application/vnd.evenfinancial.v1+json'
-    	},
-    	json:content },
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body['uuid']);
-            uuid = body['uuid'];
-        }
-        else {
-        	console.log("error: " + error)
-            console.log("response.statusCode: " + response.statusCode)
-            console.log("response.statusText: " + response.statusText)
-        }
-    }
-);
-getUrl = 'https://api.evenfinancial.com/originator/rateTables/{UUID}'
-request.get(
-  getUrl: getUrl,
-  headers: {
-        'Authorization': 'Bearer e7675dd3-ff3b-434b-95aa-70251cc3784b_88140dd4-f13e-4ce3-8322-6eaf2ee9a2d2'
-      },
+function postInfo() {
+  return new Promise((resolve, reject) => {
+     request.post({ 
+        url: url,
+        headers: {
+          'Authorization': 'Bearer e7675dd3-ff3b-434b-95aa-70251cc3784b_88140dd4-f13e-4ce3-8322-6eaf2ee9a2d2',
+          'Accept': 'application/vnd.evenfinancial.v1+json'
+        },
+        json:content },
+      function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+              console.log(body['uuid']);
+              uuid = body['uuid'];
+              getUrl = `https://api.evenfinancial.com/originator/rateTables/${uuid}`;
+              console.log(getUrl);
+          }
+          else {
+            console.log("error: " + error)
+              console.log("response.statusCode: " + response.statusCode)
+              console.log("response.statusText: " + response.statusText)
+          }
+      });
+  });
+};
 
-  )
+postInfo().then(() => {
+  request.get({
+        getUrl: getUrl,
+        headers: {
+            'Authorization': 'Bearer e7675dd3-ff3b-434b-95aa-70251cc3784b_88140dd4-f13e-4ce3-8322-6eaf2ee9a2d2',
+            'Accept': 'application/vnd.evenfinancial.v1+json'
+        },
+      },
+      function(err, res, body) {
+        if (!error && response.statusCode == 200) {
+                console.log(body);
+            }
+        else {
+           console.log("error: " + error)
+           console.log("response.statusCode: " + response.statusCode)
+           console.log("response.statusText: " + response.statusText)
+       }
+    });
+});
 
