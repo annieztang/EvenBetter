e5f8d8abed4eb49476c5ff96521973c8296884ee
William Zulueta <williamzulueta@rocketmail.com>
2019-04-13 20:08:25 -0700

    EvenBetter - Fix request fields

--- api/src/main/java/def/hacks/even/api/FinancialInformation.java
+++ api/src/main/java/def/hacks/even/api/FinancialInformation.java
@@ -2,26 +2,29 @@ package def.hacks.even.api;
 
 public class FinancialInformation {
     public enum EmploymentStatus {
-        employed,military, not_employed, other, retired, self_employed
+        employed, military, not_employed, other, retired, self_employed
 
     }
-    public EmploymentStatus employmentStatus;
+
+    public String employmentStatus;
 
     public enum EmploymentPayFrequency {
         weekly, biweekly, twice_monthly, monthly
     }
-    public EmploymentPayFrequency employmentPayFrequency;
 
-    public long annualIncome;
-    public long monthlyNetIncome;
+    public String employmentPayFrequency;
+
+    public int annualIncome;
+    public int monthlyNetIncome;
     public String bankName;
-    public long bankRoutingNumber;
+    public String bankRoutingNumber;
 
     public enum BankAccountType {
         checking, savings, other
     }
-    public BankAccountType bankAccountType;
+
+    public String bankAccountType;
 
     public int monthsAtBank;
-    public long bankAccountNumeber;
+    public String bankAccountNumber;
 }
