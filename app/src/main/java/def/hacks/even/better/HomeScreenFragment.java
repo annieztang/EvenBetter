8a7e07dada6ce5e47ff784df37226a409e44ec7c
William Zulueta <williamzulueta@rocketmail.com>
2019-04-14 06:51:26 -0700

    EvenBetter - Add transitions between fragments

--- app/src/main/java/def/hacks/even/better/HomeScreenFragment.java
+++ app/src/main/java/def/hacks/even/better/HomeScreenFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.transition.Slide;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,7 +31,10 @@ public class HomeScreenFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                transaction.add(R.id.frameLayout, new ChooseLoanFragment());
+                Fragment fragment = new ChooseLoanFragment();
+                fragment.setEnterTransition(new Slide(Gravity.RIGHT));
+                fragment.setExitTransition(new Slide(Gravity.LEFT));
+                transaction.add(R.id.frameLayout, fragment);
                 transaction.addToBackStack(ChooseLoanFragment.TAG);
                 transaction.commit();
             }
